6dd6d44fe06dcd35554544fd08541c435887c066
孙煦航 <2755325937@qq.com>
2025-11-10 00:11:52 +0800

    Add ContactDTO class for contact management

---
+++ src/main/java/com/contacts/dto/ContactDTO.java
@@ -0,0 +1,82 @@
+package com.contacts.dto;
+
+import java.time.LocalDateTime;
+
+public class ContactDTO {
+    private Long id;
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+    private String company;
+    private LocalDateTime createdTime;
+    private LocalDateTime updatedTime;
+    
+    // 构造方法
+    public ContactDTO() {}
+    
+    // Getter和Setter方法
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getPhone() {
+        return phone;
+    }
+    
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+    
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public String getCompany() {
+        return company;
+    }
+    
+    public void setCompany(String company) {
+        this.company = company;
+    }
+    
+    public LocalDateTime getCreatedTime() {
+        return createdTime;
+    }
+    
+    public void setCreatedTime(LocalDateTime createdTime) {
+        this.createdTime = createdTime;
+    }
+    
+    public LocalDateTime getUpdatedTime() {
+        return updatedTime;
+    }
+    
+    public void setUpdatedTime(LocalDateTime updatedTime) {
+        this.updatedTime = updatedTime;
+    }
+}
